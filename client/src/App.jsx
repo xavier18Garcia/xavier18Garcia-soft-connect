4222f74b8a438efa79f4708faf2ef6f806ebd32d
xavier18Garcia <ag516588@gmail.com>
2025-10-13 20:02:19 -0500

    Interacion de rutas mas taildwincss

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { RoutesConfig } from './routes/config.Routes.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+	return (
+		<Router>
+			<RoutesConfig />
+		</Router>
+	)
+}
+
+export default App
