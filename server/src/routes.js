cf8db73f2f556b7b387f49fdc8fef0b67ae2937a
ANHELL0S <angelogarcia@mailes.ueb.edu.ec>
2025-10-13 23:54:03 -0500

    feat: implement answer in routes and index models

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,10 +4,12 @@ import { API_VS } from './common/constants/prefixAPi-const.js'
 import authRouter from './core/routes/auth-route.js'
 import usersRouter from './core/routes/user-route.js'
 import postsRouter from './core/routes/post-route.js'
+import answersRouter from './core/routes/answer-route.js'
 
 export const setupRoutes = app => {
 	app.use(API_VS + '/auth', authRouter)
 	app.use(API_VS + '/users', usersRouter)
 	app.use(API_VS + '/posts', postsRouter)
+	app.use(API_VS + '/answers', answersRouter)
 	swaggerDocs(app)
 }
